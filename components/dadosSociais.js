c4c578394b60715d5d562675522202c1fad0773f
pwcodigo <pwcodigo@email.com>
2020-05-23 15:59:29 -0300

    feat: adicionei novo componente com dados sociais

---
+++ components/dadosSociais.js
@@ -0,0 +1,28 @@
+export default {
+    name: "dadosSociais",
+    template: `  
+    <div class="container-fluid">
+    <!-- Essa parte do código para centralizar footer -->
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">                    
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                    <span style="font-size:14px"> Código Desenvolvido por <strong>PW</strong></PW>CÒDIGO </span> - 
+                    <a style="color: inherit;" target="_blank" href="https://github.com/pwcodigo/valordaacao_vue_usando_click">
+                        <i class="fa fa-github-square" aria-hidden="true" style="vertical-align: middle;font-size:30px"></i>
+                    </a>
+                    <a style="color: inherit" href="https://www.instagram.com/pwcodigo/" target="_blank">
+                        <i class="fa fa-instagram" aria-hidden="true" style="vertical-align: middle;font-size:30px" ></i>
+                    </a>
+
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">                    
+                    
+                </div>
+               
+            </div>
+        </div>
+    </div>
+    `
+}
